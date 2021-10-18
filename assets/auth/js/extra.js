3878e9087acd9438fc7765613c9dbd48c8063e19
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 23:17:30 +0300

    asset components

---
+++ assets/auth/js/extra.js
@@ -0,0 +1,2 @@
+function bindPasswordResetForm(){$('form.reset-password-form').on('submit',function(e){e.preventDefault();$('.reset-form').removeClass('d-block').addClass('d-none');$('.reset-confirmation').addClass('d-block');});}
+window.addEventListener('load',function(){bindPasswordResetForm();});
