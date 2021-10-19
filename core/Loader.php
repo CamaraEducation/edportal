67a2db3ded38afc4afe8c44f9beee8d3d8d10b55
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-19 20:26:14 +0300

    stylesheet loader function

--- core/Loader.php
+++ core/Loader.php
@@ -22,7 +22,28 @@ function config($conf){
 	return $env;
 }
 
+function theme(){
+	setcookie('sidebarBg', 'color_1', time() + (180), "/");
+	setcookie('headerPosition', 'fixed', time() + (180), "/");
+	setcookie('containerLayout', 'full', time() + (180), "/");
+	setcookie('direction', 'ltr', time() + (180), "/");
+	setcookie('sidebarPosition', 'fixed', time() + (180), "/");
+	setcookie('layout', 'horizontal', time() + (180), "/");
+	setcookie('sidebarStyle', 'icon-hover', time() + (180), "/");
+	setcookie('primary', 'color_12', time() + (180), "/");
+	setcookie('headerBg', 'color_13', time() + (180), "/");
+	setcookie('logo_src', 'images/logo-white.png', time() + (180), "/");
+	setcookie('navheaderBg', 'color_2', time() + (180), "/");
+	setcookie('typography', 'HelveticaNeue', time() + (180), "/");
+}
+
+if (!isset($_COOKIE['loaded'])){
+	setcookie('loaded', 'true', time() + (86400 * 30), "/");
+	header("Refresh:1");	
+}
+
 require_once 'Configuration.php';
 require_once 'Notices.php';
 load_classphp('controls');
+theme();
 ?>
