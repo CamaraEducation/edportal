58dcca1c281222e889d2592ff19ba9921e3012ed
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-07 00:41:37 +0300

    dynamic menu

--- views/layout/sidebar.blade.php
+++ views/layout/sidebar.blade.php
@@ -21,11 +21,16 @@
                     <span class="nav-text">Applications</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="app-profile.html"></a></li>
+                    <li><a href="/videos">Video</a></li>
+                    <li><a href="/docs">Publication</a></li>
+                    @php $application = AppsControl::fetch() @endphp
+                    @foreach ($application as $app)
+                    <li><a href="/view/app/{{$app['id']}}">{{$app['name']}}</a></li>   
+                    @endforeach
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                <i class="flaticon-077-menu-1"></i>
+                <i class="flaticon-061-puzzle"></i>
                     <span class="nav-text">Management</span>
                 </a>
                 <ul aria-expanded="false">
@@ -33,16 +38,22 @@
                 </ul>
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                <i class="flaticon-077-menu-1"></i>
+                <i class="flaticon-381-settings-2"></i>
                     <span class="nav-text">Administration</span>
                 </a>
                 <ul aria-expanded="false">
                     <li><a href="/add/app">Applications</a></li>
+                    <li><a href="javascript:void()">video</a>
+                        <ul aria-expanded="false">
+                            <li><a href="/stats/video">statistics</a></li>
+                            <li><a href="/add/video">Upload</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </li>
             <li>
                 <a class="ai-icon" href="https://camtics.camara.org" aria-expanded="false">
-                    <i class="flaticon-144-layout"></i>
+                    <i class="flaticon-049-copy"></i>
                     <span class="nav-text">Support</span>
                 </a>
             </li>            
