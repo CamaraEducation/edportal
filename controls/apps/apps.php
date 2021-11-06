bf6a4510b510976db64aebd3d0813cbe8897797b
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-06 19:44:19 +0300

    Apps controller

---
+++ controls/apps/apps.php
@@ -0,0 +1,27 @@
+<?php
+class AppsControl{
+    public static function fetch(){
+        $sql = "SELECT * FROM apps";
+        $sql = mysqli_query(conn(), $sql);
+        $apps = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+        return $apps;
+    }
+
+    public static function create(){
+        $app_name = mysqli_real_escape_string(conn(), $_POST['app_name']);
+        $app_type = mysqli_real_escape_string(conn(), $_POST['app_type']);
+        $app_path = mysqli_real_escape_string(conn(), $_POST['app_path']);
+        $description = mysqli_real_escape_string(conn(), $_POST['description']);
+
+        $banner = '/' . FileUploader::upload($file='thumbnail', $dir='apps/');
+
+        $sql = "INSERT INTO apps VALUES (DEFAULT, '$app_name', '$app_type', '$app_path', '$description', '$banner')";
+        if(mysqli_query(conn(), $sql)){
+            header('Location: /apps');
+        }else{
+           echo 'something wrong';
+        }
+    }
+}
+?>
