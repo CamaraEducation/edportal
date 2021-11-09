d15872eddefaf0627ae0ef0ee2475e66d9d560e9
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-09 22:24:51 +0300

    fetch added video

--- views/video/list.blade.php
+++ views/video/list.blade.php
@@ -3,7 +3,7 @@
 @section('header')
     <style>
         .video-bg{
-            background: url(/upload/apps/cover.jpg) center no-repeat;
+            background-size: cover !important;
             height: 200px;
             width: 100%;
         }
@@ -20,52 +20,32 @@
     <div class="content-body">
         <div class="container-fluid">
             <div class="row">
+                @php $videos = VideosControl::fetch(); @endphp
+                @foreach ($videos as $video)
                 <div class="col-xl-3 video-card">
                     <div class="card mb-3">
-                        <div id="over" class="video-bg" alt="Card image cap">
-                            <img class="play" src="/assets/img/play.png" alt="">
+                        <div id="over" class="video-bg" alt="Card image cap" style="background: url({{$video['thumbnail']}}) center no-repeat;">
+                            <a href="/view/video/{{$video['id']}}/{{str_replace(' ', '-', $video['title'])}}">
+                                <img class="play" src="/assets/img/play.png" alt="play">
+                            </a>
                         </div>
                         <div class="card-header">
-                            <h5 class="card-title">Big Buck bunny</h5>
+                            <a href="/view/video/{{$video['id']}}/{{str_replace(' ', '-', $video['title'])}}">
+                                <h5 class="card-title">{{$video['title']}}</h5>
+                            </a>
                         </div>
                         <div class="card-footer">
-                            <p class="card-text d-inline"><i class="fa fa-eye"> 150 view</i></p>
-                            <a href="javascript:void(0);" class="card-link float-right">2 months ago</a>
+                            <p class="card-text d-inline">
+                                <i class="fa fa-eye"></i> 
+                                {{VideosControl::views($video['id'])}}
+                            </p>
+                            <a href="javascript:void(0);" class="card-link float-right">
+                                {{span_count($video['span'])}}
+                            </a>
                         </div>
                     </div>
                 </div>
-
-                
-                <div class="col-xl-3 video-card">
-                    <div class="card mb-3">
-                        <div id="over" class="video-bg" alt="Card image cap">
-                            <img class="play" src="/assets/img/play.png" alt="">
-                        </div>
-                        <div class="card-header">
-                            <h5 class="card-title">Big Buck bunny</h5>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-text d-inline"><i class="fa fa-eye"> 150 view</i></p>
-                            <a href="javascript:void(0);" class="card-link float-right">2 months ago</a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-xl-3 video-card">
-                    <div class="card mb-3">
-                        <div id="over" class="video-bg" alt="Card image cap">
-                            <img class="play" src="/assets/img/play.png" alt="">
-                        </div>
-                        <div class="card-header">
-                            <h5 class="card-title">Big Buck bunny</h5>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-text d-inline"><i class="fa fa-eye"> 150 view</i></p>
-                            <a href="javascript:void(0);" class="card-link float-right">2 months ago</a>
-                        </div>
-                    </div>
-                </div>
-
+                @endforeach
             </div>
         </div>
     </div>    
