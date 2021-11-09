f1e10b91a8edd7d5f0797e6a877f79c839386885
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-09 22:21:46 +0300

    session identifier

--- controls/auth/session.php
+++ controls/auth/session.php
@@ -8,6 +8,7 @@ class Session{
         $user   = strtolower($user);
         $data = "select * from users where username = '$user' or user_email = '$user'";
         $data   = mysqli_query(conn(), $data);
+        $_SESSION['identifier'] = $_SERVER['REQUEST_TIME'];
 
 		//iterate the attained data to create a session
         foreach(mysqli_fetch_assoc($data) as $key=>$value){
