f10779edfcf36aef469bb89c87cda3f9df76b971
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-10 20:22:36 +0300

    document categories

--- controls/public/category.php
+++ controls/public/category.php
@@ -4,11 +4,19 @@ class Category{
 
     }
 
-    public static function fetch(){
-        $sql = "SELECT * FROM video_category ORDER BY name";
-        $sql = mysqli_query(conn(), $sql);
+    public static function fetch($type = 'video'){
+        switch($type){
+            case 'video':
+                $sql = "SELECT * FROM video_category ORDER BY name";
+                $sql = mysqli_query(conn(), $sql); break;
+            case 'doc':
+                $sql = "SELECT * FROM doc_category ORDER BY name";
+                $sql = mysqli_query(conn(), $sql);
+            break;
+        }
 
-        return mysqli_fetch_all($sql, MYSQLI_ASSOC);
+            return mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
     }
 
     public static function create(){
