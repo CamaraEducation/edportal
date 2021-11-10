874d11992a5c728df35b70c797571c9f0c39085f
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-10 20:23:51 +0300

    statistics user, video, ducment

--- controls/stats/statistic.php
+++ controls/stats/statistic.php
@@ -7,5 +7,26 @@ class StatsController{
 
         return $video['views'];
     }
+
+    public static function count_user(){
+        $sql = "SELECT count(id) as users FROM users";
+        $sql = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+        
+        return $sql['users'];
+    }
+
+    public static function count_video(){
+        $sql = "SELECT count(id) as videos FROM video";
+        $sql = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+        
+        return $sql['videos'];
+    }
+
+    public static function count_document(){
+        $sql = "SELECT count(id) as docs FROM document";
+        $sql = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+        
+        return $sql['docs'];
+    }
 }
 ?>
