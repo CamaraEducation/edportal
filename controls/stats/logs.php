0ae56e267a4ea1581dbfece60c20be9c965d92f2
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-10 20:23:32 +0300

    video views loger

--- controls/stats/logs.php
+++ controls/stats/logs.php
@@ -2,9 +2,10 @@
 class LogsController{
     public static function log_video_view($id){
         $user = 1;
-        $sql = "SELECT count(id) FROM video_views WHERE video_id = '$id' AND user_id = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
-        if(mysqli_num_rows(mysqli_query(conn(), $sql)) == 0){
-            $log = "INSERT INTO video_views VALUES ('$id', '$user')";
+        $sql = "SELECT count(id) as views FROM video_views WHERE video_id = '$id' AND user_id = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
+        $data = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+        if($data['views'] == 0){
+            $log = "INSERT INTO video_views (video_id, user_id) VALUES ('$id', '$user')";
             mysqli_query(conn(), $log);
         }
     }
