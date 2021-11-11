4eca8b15928a70590d9b7aeba2951da843dd10d2
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-11 16:20:41 +0300

    create user profile

---
+++ views/user/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layout.app')
+@section('title', 'Create User')
+@section('header') @endsection
+@section('content')
+    <div class="content-body">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12">
+					<div class="card">
+						<div class="card-header">
+							<h2 class="card-title mb-2 text-primary">Create New User</h2>
+						</div>
+						<div class="card-body">
+							<div class="basic-form">
+								<form id="add_video" name="add_video" action="/create/user" method="POST" onsubmit="return submitted()">
+									<div class="form-row">
+										<div class="form-group col-md-6">
+											<label>First Name</label>
+											<input type="text" name="fname" id="fname" class="form-control" placeholder="First Name eg. Hamisi">
+										</div>
+										<div class="form-group col-md-6">
+											<label>Last Name</label>
+											<input type="text" name="lname" id="lname" class="form-control" placeholder="Last Name eg. John">
+										</div>
+									</div>
+									<div class="form-row">
+										<div class="form-group col-md-6">
+											<label>Username / id Number</label>
+											<input type="text" name="username" id="username" class="form-control" placeholder="ie. registration id eg. ST001, T4743 etc." >
+										</div>
+										<div class="form-group col-md-6">
+											<label>User Role</label>
+											<select name="role" id="role" class="form-control default-select" >
+												<option value="" hidden>select User Type</option>
+                                                <option value="1">Technician</option>
+												<option value="2">Head of School</option>
+												<option value="3">Dep. Head of School</option>
+                                                <option value="4">Academic</option>
+                                                <option value="5">Teacher</option>
+                                                <option value="6">Student</option>
+											</select>
+										</div>
+									</div>
+									<div class="form-row">
+										<div class="form-group col-md-6">
+											<label>User Gender</label>
+											<select name="gender" id="gender" class="form-control default-select" >
+												<option value="" hidden>select User Gender</option>
+                                                <option value="male">male</option>
+												<option value="female">female</option>
+											</select>
+										</div>
+										<div class="form-group col-md-6">
+											<label>Password</label>
+											<input type="text" name="pass" id="pass" class="form-control" value="123456" disabled>
+										</div>
+									</div>
+									<button type="submit" id="submit" class="btn btn-primary">Upload</button>
+								<form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+@section('footer') @endsection
