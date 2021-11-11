0045f6021814841e8498f5214285f423031ead50
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-11 16:19:50 +0300

    user controller class

---
+++ controls/users/user.php
@@ -0,0 +1,29 @@
+<?php
+class UsersControl{
+    public static function get($id){
+        $sql = "SELECT * FROM users WHERE id='$id'";
+        return mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+    }
+
+    public static function fetch_staff(){
+        $sql = "SELECT * FROM users WHERE user_role<'6'";
+        return mysqli_fetch_all(mysqli_query(conn(), $sql), MYSQLI_ASSOC);
+    }
+
+    public static function fetch_student(){
+        $sql = "SELECT * FROM users WHERE user_role='6'";
+        return mysqli_fetch_all(mysqli_query(conn(), $sql), MYSQLI_ASSOC);
+    }
+
+    public static function create(){
+        foreach($_POST as $key => $value){ $$key = mysqli_real_escape_string(conn(), $value); } $pass = md5($pass);
+        $sql = "INSERT INTO users (username, first_name, last_name, user_role, user_pass, user_gender) VALUES ('$username', '$fname', '$lname', '$role', '$pass')";
+        mysqli_query(conn(), $sql) ? header('Location: /users') : 'something is wrong';
+    }
+
+    public static function delete($id){
+        $sql = "DELETE FROM users WHERE id='$id'";
+        mysqli_query(conn(), $sql) ? header('Location: /users') : 'something is wrong';
+    }
+}
+?>
