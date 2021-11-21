435fb858988fa8ca9f7f6e327c8fb9d95254c2c9
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-21 08:55:28 +0300

    notice directory

--- core/Notices.php
+++ core/Notices.php
@@ -4,7 +4,7 @@ function notice($page){
 		case 'auth':
 			switch ($_COOKIE['notice']){
 				case 'df_reg_200':
-					return ['subject'=>'Registration Successful ', 'message'=>'Congraturalation, You have successfuly registered, Go home','button'=>' Go Home', 'link'=>'/home'];
+					return ['subject'=>'Registration Successful ', 'message'=>'Congraturalation, You have successfuly registered, Go home','button'=>' Go Home', 'link'=>'/'];
 					break;
 				case 'df_reg_209':
 					return ['subject'=>'Registration Unsuccesful', 'message'=>'Registration was unsuccesful, Try Again ', 'button'=>'Try Again', 'link'=>'/register'];
@@ -16,15 +16,15 @@ function notice($page){
 					return ['subject'=>'Registration Unsuccesful', 'message'=>'Email used is already registered on our system','button'=>'Try Again', 'link'=>'/register'];
 					break;
 				case 'df_log_200':
-					return ['subject'=>'Succesfully Authenticated', 'message'=>'You have succesfully loged in to your Account', 'button'=>'Proceed', 'link'=>'/home'];
+					return ['subject'=>'Succesfully Authenticated', 'message'=>'You have succesfully loged in to your Account', 'button'=>'Proceed', 'link'=>'/'];
 					break;
 				case 'df_log_201':
 					return ['subject'=>'Authentication Failed', 'message'=>'Could not authorize credentials provided','button'=>'Try Again', 'link'=>'/login'];
 					break;
 				default:
-					return ['subject'=>'No Request', 'message'=>'No authentication request was Made','button'=>'Go Home', 'link'=>'/home'];
+					return ['subject'=>'No Request', 'message'=>'No authentication request was Made','button'=>'Go Home', 'link'=>'/'];
 			}
 		default:
-			return ['subject'=>'Oppsie', 'message'=>'seems you\'re trying to be a genius','button'=>'Go Home', 'link'=>'/home'];
+			return ['subject'=>'Oppsie', 'message'=>'seems you\'re trying to be a genius','button'=>'Go Home', 'link'=>'/'];
 	}
 }
