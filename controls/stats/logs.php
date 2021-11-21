cf5395f91bd68492bf0eb7a6d4680adb963bb5e2
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-21 08:54:52 +0300

    logs manager

--- controls/stats/logs.php
+++ controls/stats/logs.php
@@ -1,7 +1,7 @@
 <?php
 class LogsController{
     public static function log_video_view($id){
-        $user = 1;
+        $user = $_SESSION['id'];
         $sql = "SELECT count(id) as views FROM video_views WHERE video_id = '$id' AND user_id = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
         $data = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
         if($data['views'] == 0){
@@ -11,14 +11,23 @@ class LogsController{
     }
 
     public static function log_page_visit(){
-        $page = $_SERVER['REQUEST_URI'];
-        $time = 123456789;
-        $user = 1;
-        $sql = "SELECT count(id) FROM page_visit WHERE page = '$page' AND visitor = '$user' AND time = '$time'";
-        if(mysqli_num_rows(mysqli_query(conn(), $sql)) == 0){
-            $log = "INSERT INTO page_visit VALUES ('$user', '$page', '$time')";
+        $page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
+        $user = $_SESSION['id'];
+        $sql = "SELECT count(id) as views FROM page_visit WHERE page = '$page' AND visitor = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
+        $data = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+        if($data['views'] == 0){
+            $log = "INSERT INTO page_visit VALUES (DEFAULT, '$user', '$page', DEFAULT, DEFAULT)";
             mysqli_query(conn(), $log);
         }
+    }
+
+    public static function log_live_time(){
+        LogsController::log_page_visit();
+        $page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
+        $user = $_SESSION['id'];
+        $duration = $_POST['duration'];
+        $sql= "UPDATE page_visit SET live = '$duration' WHERE page = '$page' AND visitor = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
+        mysqli_query(conn(), $sql);
     }
 }
 ?>
