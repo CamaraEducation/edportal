85521d9c8d4a754597c0f9670e029644050f6751
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-30 12:23:47 +0300

    bug fix with profile view

--- views/layout/sidebar.blade.php
+++ views/layout/sidebar.blade.php
@@ -1,13 +1,5 @@
 <div class="deznav">
     <div class="deznav-scroll">
-        <div class="main-profile">
-            <div class="image-bx">
-                <img src="/assets/img/favicon.png" alt="">
-                <a href="javascript:void(0);"><i class="fa fa-cog" aria-hidden="true"></i></a>
-            </div>
-            <h5 class="name"><span class="font-w400">Hello,</span> Marquez</h5>
-            <p class="email">marquezzzz@mail.com</p>
-        </div>
         <ul class="metismenu" id="menu">
             <li class="nav-label first">Main Menu</li>
             <li>
