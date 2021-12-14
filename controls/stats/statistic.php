a288567e309122acdaf3a3b4b68e369bade4dcfb
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-12-14 06:21:41 +0300

    page visit and log stats

--- controls/stats/statistic.php
+++ controls/stats/statistic.php
@@ -28,5 +28,23 @@ class StatsController{
         
         return $sql['docs'];
     }
+
+    public static function count_app_visits(){
+        $uri = substr($_SERVER['REQUEST_URI'], 1);
+        $sql = "SELECT COUNT(uri) AS all_time, 
+                (SELECT COUNT(uri) FROM page_visit WHERE uri='$uri' AND MONTH(TIME) = MONTH(CURRENT_TIMESTAMP)) AS month_time,
+                (SELECT COUNT(uri) FROM page_visit WHERE URI='$uri' AND DATE(TIME) = DATE(CURRENT_TIMESTAMP)) AS today
+            FROM page_visit WHERE URI='$uri'";
+
+        return mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+    }
+
+    public static function count_video_view(){
+        $sql = "SELECT COUNT(id) AS all_time, 
+            (SELECT COUNT(id) FROM video_views WHERE MONTH(TIME) = MONTH(CURRENT_TIMESTAMP)) AS monthly,
+            (SELECT COUNT(id) FROM video_views WHERE DATE(TIME) = DATE(CURRENT_TIMESTAMP)) AS today
+        FROM video_views";
+        return mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+    }
 }
 ?>
