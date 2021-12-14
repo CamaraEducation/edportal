c90458f7ae22457da57c1435ec1a9ad757df3e73
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-12-14 06:25:50 +0300

    view pdf with a flipping tool

--- views/document/view.blade.php
+++ views/document/view.blade.php
@@ -1,24 +1,23 @@
-@php
-	$doc = DocsControl::get($id);
-@endphp
+@php $doc = DocsControl::get($id); @endphp
 @extends('layout.app')
 @section('title', $doc['name'])
-@section('header')
-	<style>
-		div#container1 {
-			height: 100vh;
-		}
-	</style>
 
+@section('header')
+	<link rel="stylesheet" href="https://previews.customer.envatousercontent.com/files/308385206/css/animate.min.css" type="text/css">
 @endsection
+
 @section('content')
 	<div class="content-body">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="profile card card-body px-3 pt-3 pb-0">
-							<div class="container" id="container1">
-
+							<div class="container">
+								<div id='page-trigger' class="text-center">
+									<div class="service-box" style="padding-bottom: 30px">
+										<a href="#" class="btn btn-primary">Read the Book</a>
+									</div>
+								</div>
 							</div>							  
 						</div>
 					</div>
@@ -32,70 +31,21 @@
 										<ul class="nav nav-tabs">
 											<li class="nav-item"><a href="#about-me" data-toggle="tab" class="nav-link active show">Description</a>
 											</li>
-											<li class="nav-item"><a href="#profile-settings" data-toggle="tab" class="nav-link">Setting</a>
+											<li class="nav-item"><a href="#profile-settings" data-toggle="tab" class="nav-link">Statistics</a>
 											</li>
 										</ul>
 										<div class="tab-content">
 											<div id="about-me" class="tab-pane fade active show">
 												<div class="profile-about-me">
 													<div class="pt-4 border-bottom-1 pb-3">
-														<h4 class="text-primary">Application Description</h4>
-														<p class="mb-2">big bunny</p>
+														<h4 class="text-primary">{{$doc['name']}}</h4>
+														<p class="mb-2">{{$doc['description']}} </p>
 													</div>
 												</div>
 											</div>
 											<div id="profile-settings" class="tab-pane fade">
 												<div class="pt-3">
-													<div class="settings-form">
-														<h4 class="text-primary">Account Setting</h4>
-														<form>
-															<div class="form-row">
-																<div class="form-group col-md-6">
-																	<label>Email</label>
-																	<input type="email" placeholder="Email" class="form-control">
-																</div>
-																<div class="form-group col-md-6">
-																	<label>Password</label>
-																	<input type="password" placeholder="Password" class="form-control">
-																</div>
-															</div>
-															<div class="form-group">
-																<label>Address</label>
-																<input type="text" placeholder="1234 Main St" class="form-control">
-															</div>
-															<div class="form-group">
-																<label>Address 2</label>
-																<input type="text" placeholder="Apartment, studio, or floor" class="form-control">
-															</div>
-															<div class="form-row">
-																<div class="form-group col-md-6">
-																	<label>City</label>
-																	<input type="text" class="form-control">
-																</div>
-																<div class="form-group col-md-4">
-																	<label>State</label>
-																	<select class="form-control default-select" id="inputState">
-																		<option selected="">Choose...</option>
-																		<option>Option 1</option>
-																		<option>Option 2</option>
-																		<option>Option 3</option>
-																	</select>
-																</div>
-																<div class="form-group col-md-2">
-																	<label>Zip</label>
-																	<input type="text" class="form-control">
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="custom-control custom-checkbox">
-																	<input type="checkbox" class="custom-control-input" id="gridCheck">
-																	<label class="custom-control-label" for="gridCheck"> Check me out</label>
-																</div>
-															</div>
-															<button class="btn btn-primary" type="submit">Sign
-																in</button>
-														</form>
-													</div>
+													...
 												</div>
 											</div>
 										</div>
@@ -106,36 +56,87 @@
 					</div>
 				</div>
 			</div>
+
+			<div id='page'></div>
 		</div>
 @endsection
 @section('footer')
-	@js('/assets/vendor/flipjs/js/jquery.min.js')
-    @js('/assets/vendor/flipjs/js/html2canvas.min.js')
-    @js('/assets/vendor/flipjs/js/pdf.worker.js')
-    @js('/assets/vendor/flipjs/js/three.min.js')
-    @js('/assets/vendor/flipjs/js/pdf.min.js')
-    @js('/assets/vendor/flipjs/js/3dflipbook.min.js')
+		<!-- wow flip plugin stylesheet -->
+		@js('/assets/vendor/flipper/jquery.easing.min.js')
+		@js('/assets/vendor/flipper/jquery.fittext.js')
+		@js('/assets/vendor/flipper/wow.min.js')
+		@css('/assets/vendor/flipper/wow_book.css')
 
-	<script type="text/javascript">
+		<style>
+			.wowbook-right .wowbook-gutter-shadow {
+			background-image: url("/assets/vendor/flipper/gutter_shadow_right.png");
+			background-position: 0 0;
+			width: 75px;
+			}
+			.wowbook-left .wowbook-gutter-shadow {
+			background-image: url("/assets/vendor/flipper/gutter_shadow_left.png");
+			opacity: 0.5;
+			width: 60px;
+			}
+			.wowbook-control-currentPage {
+			font-family: "Segoe UI",Helvetica,Arial,sans-serif;
+			}
+		</style>
 
-		var template = {
-		  html: '/assets/vendor/flipjs/templates/book.html',
-		  links: [{
-			rel: 'stylesheet',
-			href: '/assets/vendor/flipjs/css/font-awesome.min.css'
-		  }],
-		  styles: [
-			'/assets/vendor/flipjs/css/short-black-book-view.css'
-		  ],
-		  script: '/assets/vendor/flipjs/js/default-book-view.js'
-		};
-  
-		// Book 1
-		$('#container1').FlipBook({
-		  pdf: '{{$doc["source"]}}',
-		  template: template
-		});
+	
+		@js('/assets/vendor/flipper/pdf.combined.min.js')
+		@js('/assets/vendor/flipper/wow_book.min.js')
 		
-  
-	</script>
+		<script type="text/javascript">
+			$(function(){
+			
+			    function fullscreenErrorHandler(){
+			        if (self!=top) return "The frame is blocking full screen mode. Click on 'remove frame' button above and try to go full screen again."
+			    }
+			
+			    var optionspage = {
+			         height   : 1024
+			        ,width    : 725*2
+			        // ,maxWidth : 800
+			        // ,maxHeight : 400
+			        ,pageNumbers: false
+			
+			        ,pdf: "{{$doc['source']}}"
+			        ,pdfFind: true
+			        ,pdfTextSelectable: true
+			
+			        ,lightbox : "#page-trigger"
+			        ,lightboxClass : "lightbox-pdf"
+			        ,centeredWhenClosed : true
+			        ,hardcovers : true
+			        ,curl: false
+			        ,toolbar: "lastLeft, left, currentPage, right, lastRight, find, toc, zoomin, zoomout, fullscreen"
+			        //,thumbnailsPosition : 'bottom'
+			        ,responsiveHandleWidth : 50
+			        ,onFullscreenError: fullscreenErrorHandler
+			    };
+			
+			    var books = {
+			        "#page" : optionspage
+			    };
+
+			    $("#page-trigger").on("click",function(){
+			        buildBook( "#"+this.id.replace("-trigger", "") );
+			    })
+			
+			    function buildBook( elem ){
+			        var book=$.wowBook(elem);
+			        if (!book) {
+			            $(elem).wowBook( books[elem] );
+			            book=$.wowBook(elem);
+			        }
+			        // book.opts.onHideLightbox = function(){
+			        //     setTimeout( function(){ book.destroy(); }, 1000);
+			        // }
+			        book.showLightbox();
+			    }
+			
+			
+			});
+		</script>
 @endsection
