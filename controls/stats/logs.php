290d96d145456322c5884f84933d2c15a0528d0f
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-12-14 06:21:21 +0300

    page and asset loging

--- controls/stats/logs.php
+++ controls/stats/logs.php
@@ -1,33 +1,38 @@
 <?php
 class LogsController{
-    public static function log_video_view($id){
-        $user = $_SESSION['id'];
-        $sql = "SELECT count(id) as views FROM video_views WHERE video_id = '$id' AND user_id = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
-        $data = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
-        if($data['views'] == 0){
-            $log = "INSERT INTO video_views (video_id, user_id) VALUES ('$id', '$user')";
-            mysqli_query(conn(), $log);
-        }
-    }
+	public static function log_video_view($id){
+		$user = $_SESSION['id'];
+		$log = "INSERT INTO video_views (video_id, user_id) VALUES ('$id', '$user')";
+		mysqli_query(conn(), $log);
+	}
 
-    public static function log_page_visit(){
-        $page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
-        $user = $_SESSION['id'];
-        $sql = "SELECT count(id) as views FROM page_visit WHERE page = '$page' AND visitor = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
-        $data = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
-        if($data['views'] == 0){
-            $log = "INSERT INTO page_visit VALUES (DEFAULT, '$user', '$page', DEFAULT, DEFAULT)";
-            mysqli_query(conn(), $log);
-        }
-    }
+	public static function log_page_visit(){
+		$page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
+		$user = $_SESSION['id'];
+		$log = "INSERT INTO page_visit VALUES (DEFAULT, '$user', '$page', DEFAULT, DEFAULT)";
+		mysqli_query(conn(), $log);
+	}
 
-    public static function log_live_time(){
-        LogsController::log_page_visit();
-        $page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
-        $user = $_SESSION['id'];
-        $duration = $_POST['duration'];
-        $sql= "UPDATE page_visit SET live = '$duration' WHERE page = '$page' AND visitor = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
-        mysqli_query(conn(), $sql);
-    }
+	public static function log_live_time(){
+		LogsController::log_page_visit();
+		$user = $_SESSION['id'];
+		$page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
+		$duration = $_POST['duration'];
+		$sql= "UPDATE page_visit SET live = '$duration' WHERE uri = '$page' AND visitor = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
+		mysqli_query(conn(), $sql);
+	}
+
+	public static function log_app_visit(){
+		$user   = mysqli_real_escape_string(conn(), $_POST['user_id']);
+		$app    = mysqli_real_escape_string(conn(), $_POST['app_id']);
+		$page   = mysqli_real_escape_string(conn(), $_POST['uri']);
+
+		$log = "INSERT INTO app_visits VALUES (DEFAULT, '$app', '$user', '$page', DEFAULT)";
+		if(mysqli_query(conn(), $log)){
+			echo json_encode(array('status' => 'success'));
+		}else{
+			echo json_encode(array('status' => 'error'));
+		}
+	}
 }
 ?>
