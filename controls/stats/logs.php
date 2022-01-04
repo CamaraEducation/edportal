8dedcf9387ddbd27a5bd81dd6ed7702a8162d80c
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-01-04 11:45:34 +0300

    Logs controller

--- controls/stats/logs.php
+++ controls/stats/logs.php
@@ -9,17 +9,23 @@ class LogsController{
 	public static function log_page_visit(){
 		$page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
 		$user = $_SESSION['id'];
-		$log = "INSERT INTO page_visit VALUES (DEFAULT, '$user', '$page', DEFAULT, DEFAULT)";
+		$sess = $_SESSION['sess_id'];
+		$log = "INSERT INTO page_visit VALUES (DEFAULT, '$user', '$page', '$sess',  DEFAULT, DEFAULT)";
 		mysqli_query(conn(), $log);
 	}
 
 	public static function log_live_time(){
 		LogsController::log_page_visit();
 		$user = $_SESSION['id'];
-		$page = $_SERVER['REQUEST_URI'] == '/' ? 'home' : substr($_SERVER['REQUEST_URI'], 1);
-		$duration = $_POST['duration'];
-		$sql= "UPDATE page_visit SET live = '$duration' WHERE uri = '$page' AND visitor = '$user' AND date(time) = date(CURRENT_TIMESTAMP)";
-		mysqli_query(conn(), $sql);
+		$page = $_POST['uri'] == '/' ? 'home' : substr($_POST['uri'], 1);
+		$sess = $_SESSION['sess_id'];
+		$duration = 2900;
+		$sql= "UPDATE page_visit SET live = live+'$duration' WHERE uri = '$page' AND visitor = '$user' AND identifier = '$sess'";
+		if(mysqli_query(conn(), $sql)){
+			echo 'recorded';
+		}else{
+			echo 'not recorded';
+		}
 	}
 
 	public static function log_app_visit(){
@@ -33,6 +39,33 @@ class LogsController{
 		}else{
 			echo json_encode(array('status' => 'error'));
 		}
+	}
+
+	public static function log_video_activity(){
+		$activity = mysqli_real_escape_string(conn(), $_POST['action']);
+		$users_id = mysqli_real_escape_string(conn(), $_POST['user_id']);
+		$video_id = mysqli_real_escape_string(conn(), $_POST['video_id']);
+
+		switch($activity){
+			case 'save'	:
+				$log = "INSERT INTO video_activity VALUES (DEFAULT, '$users_id', '$video_id', 1, DEFAULT)";
+				if(mysqli_query(conn(), $log)){ echo 'video added to your bookmark list'; }
+				else{echo 'You have already bookmarked this video';} break;
+			case 'dsave' : 
+				$log = "DELETE FROM video_activity WHERE user_id = '$users_id' AND video_id = '$video_id'";
+				if(mysqli_query(conn(), $log)){ echo 'video removed from your bookmark list'; }
+				else{echo 'This video was not found in your Bookmarks';} break;
+			default:
+				echo 'error';
+		}
+	}
+
+	public static function log_log_activity(){
+		$user	= $_SESSION['id'];
+		$sess	= $_COOKIE['PHPSESSID'].$_SESSION['identifier'];
+		$sql	= "INSERT INTO log_activity VALUES (DEFAULT, '$user', '$sess', DEFAULT)";
+		mysqli_query(conn(), $sql);
+		$_SESSION['sess_id'] = $sess;
 	}
 }
 ?>
