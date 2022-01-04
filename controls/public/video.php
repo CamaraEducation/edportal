dfed5aa820477a794aa3c72c42d7f0d1312bdd69
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-01-04 11:45:23 +0300

    bug fix

--- controls/public/video.php
+++ controls/public/video.php
@@ -18,12 +18,13 @@ class VideosControl{
 		$title	  = mysqli_real_escape_string(conn(), $_POST['title']);
 		$category = mysqli_real_escape_string(conn(), $_POST['category']);
 		$class 	  = mysqli_real_escape_string(conn(), $_POST['class']);
+		$tags 	  = mysqli_real_escape_string(conn(), $_POST['tags']);
 		$description = mysqli_real_escape_string(conn(), $_POST['description']);
 
 		$banner = '/' . FileUploader::upload($file = 'thumbnail', $dir = 'video/cover/');
 		$video  = '/' . FileUploader::upload($file = 'video', $dir = 'video/source/' );
 
-		$sql = "INSERT INTO video (title, description, thumbnail, source, class, category) VALUES ('$title', '$description', '$banner', '$video', '$class', '$category')";
+		$sql = "INSERT INTO video (title, description, thumbnail, source, class, category, tags) VALUES ('$title', '$description', '$banner', '$video', '$class', '$category', '$tags')";
 		if(mysqli_query(conn(), $sql)){
 			header('Location: /video');
 		}else{
@@ -37,6 +38,43 @@ class VideosControl{
 		$video = mysqli_fetch_assoc($sql);
 
 		return $video['views'];
+	}
+
+	public static function popular(){
+		$popular = StatsController::count_popular_view(); $no = 0;
+		foreach($popular as $video){
+			$id = $video['video_id'];
+			$sql = "select title, thumbnail from video where id = '$id'";
+			$pop[$no] = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+			$pop[$no]['views'] = $video['total'];  $no++;
+		}
+		return $pop;
+	}
+
+	public static function obscured(){
+		$obscured = StatsController::count_obscured_view(); $no = 0;
+		foreach($obscured as $video){
+			$id = $video['video_id'];
+			$sql = "select title, thumbnail from video where id = '$id'";
+			$obs[$no] = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+			$obs[$no]['views'] = $video['total'];  $no++;
+		}
+		return $obs;
+	}
+
+	public static function video_activity($id){
+		$sql = "SELECT COUNT(id) as bookmark FROM  video_activity where bookmark = 1 AND video_id = '$id'";
+		$sql = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+
+		return $sql['bookmark'];
+	}
+
+	public static function video_bookmark($id){
+		$user = $_SESSION['id'];
+		$sql = "SELECT COUNT(id) as bookmark FROM  video_activity where bookmark = 1 AND video_id = '$id' AND user_id = '$user'";
+		$sql = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+
+		return $sql['bookmark'];
 	}
 }
 ?>
