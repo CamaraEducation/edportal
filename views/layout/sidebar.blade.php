8fa51721ca85f78490dcaf6d37f2846c58fcb947
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-01-04 11:48:13 +0300

    sidebar layout

--- views/layout/sidebar.blade.php
+++ views/layout/sidebar.blade.php
@@ -29,7 +29,7 @@
                     <li><a href="javascript:void()">Users</a>
                         <ul aria-expanded="false">
                             <li><a href="/users/student">Students</a></li>
-                            <li><a href="/users/staff">Staff</a></li>                            
+                            <!--li><a href="/users/staff">Staff</a></li-->                            
                             <li><a href="/add/user">Create</a></li>
                         </ul>
                     </li>
@@ -43,7 +43,12 @@
                     <span class="nav-text">Administration</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li><a href="/add/app">Applications</a></li>
+                    <li><a href="javascript:void()">Applications</a>
+                        <ul aria-expanded="false">
+                            <li><a href="/stats/app">statistics</a></li>
+                            <li><a href="/add/app">Add</a></li>
+                        </ul>
+                    </li>
                     <li><a href="javascript:void()">video</a>
                         <ul aria-expanded="false">
                             <li><a href="/stats/video">statistics</a></li>
