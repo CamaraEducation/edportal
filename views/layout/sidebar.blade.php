365eeb0b03d800bc898eb3de279526f5f4514455
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-01-20 10:53:36 +0300

    role based menu

--- views/layout/sidebar.blade.php
+++ views/layout/sidebar.blade.php
@@ -21,48 +21,11 @@
                     @endforeach
                 </ul>
             </li>
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                <i class="flaticon-061-puzzle"></i>
-                    <span class="nav-text">Management</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="javascript:void()">Users</a>
-                        <ul aria-expanded="false">
-                            <li><a href="/users/student">Students</a></li>
-                            <!--li><a href="/users/staff">Staff</a></li-->                            
-                            <li><a href="/add/user">Create</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="/create/notice">Announcement</a>
-                    </li>
-                </ul>
-            </li>
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                <i class="flaticon-381-settings-2"></i>
-                    <span class="nav-text">Administration</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="javascript:void()">Applications</a>
-                        <ul aria-expanded="false">
-                            <li><a href="/stats/app">statistics</a></li>
-                            <li><a href="/add/app">Add</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="javascript:void()">video</a>
-                        <ul aria-expanded="false">
-                            <li><a href="/stats/video">statistics</a></li>
-                            <li><a href="/add/video">Upload</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="javascript:void()">Publications</a>
-                        <ul aria-expanded="false">
-                            <li><a href="/stats/document">statistics</a></li>
-                            <li><a href="/add/document">Upload</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+            @if (viewer() == 1)
+                @include('layout.menu.admin')
+            @else @if             
+            @endif
+            
             <li>
                 <a class="ai-icon" href="https://camtics.camara.org" aria-expanded="false">
                     <i class="flaticon-049-copy"></i>
