6bce5a221a12736926c303b8d4c0c572b6ce6d85
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-02 18:52:54 +0300

    minor refactor

--- views/layout/menu/admin.blade.php
+++ views/layout/menu/admin.blade.php
@@ -1,23 +1,6 @@
 <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-    <i class="flaticon-061-puzzle"></i>
-        <span class="nav-text">Management</span>
-    </a>
-    <ul aria-expanded="false">
-        <li><a href="javascript:void()">Users</a>
-            <ul aria-expanded="false">
-                <li><a href="/users/student">Students</a></li>
-                <!--li><a href="/users/staff">Staff</a></li-->                            
-                <li><a href="/add/user">Create</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="/create/notice">Announcement</a>
-        </li>
-    </ul>
-</li>
-<li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
     <i class="flaticon-381-settings-2"></i>
-        <span class="nav-text">Administration</span>
+        <span class="nav-text">Management</span>
     </a>
     <ul aria-expanded="false">
         <li><a href="javascript:void()">Applications</a>
@@ -37,6 +20,23 @@
                 <li><a href="/stats/document">statistics</a></li>
                 <li><a href="/add/document">Upload</a></li>
             </ul>
+        </li>
+    </ul>
+</li>
+<li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+    <i class="flaticon-061-puzzle"></i>
+        <span class="nav-text">Administration</span>
+    </a>
+    <ul aria-expanded="false">
+        <li><a href="javascript:void()">Users</a>
+            <ul aria-expanded="false">
+                <li><a href="/users/student">Students</a></li>
+                <!--li><a href="/users/staff">Staff</a></li-->                            
+                <li><a href="/add/user">Create</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="/create/notice">Announcement</a>
         </li>
     </ul>
 </li>
