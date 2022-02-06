25e2fa831c3976d52da55f7f0415effcf0f0e478
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-06 20:22:58 +0300

    admin menu reorganization

--- views/layout/menu/admin.blade.php
+++ views/layout/menu/admin.blade.php
@@ -21,6 +21,13 @@
                 <li><a href="/add/document">Upload</a></li>
             </ul>
         </li>
+        <li><a href="javascript:void()">Analytics</a>
+            <ul aria-expanded="false">
+                <li><a href="/analytics/general">General</a></li>
+                <li><a href="/analytics/video">Videos</a></li>
+                <li><a href="/analytics/document">Documents</a></li>
+            </ul>
+        </li>
     </ul>
 </li>
 <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
@@ -35,8 +42,12 @@
                 <li><a href="/add/user">Create</a></li>
             </ul>
         </li>
-        <li>
-            <a href="/create/notice">Announcement</a>
+        <li><a href="javascript:void()">System</a>
+            <ul aria-expanded="false">
+                <li><a href="/users/student">Update</a></li>
+                <!--li><a href="/users/staff">Staff</a></li-->                            
+                <li><a href="/add/user">Export Data</a></li>
+            </ul>
         </li>
     </ul>
 </li>
