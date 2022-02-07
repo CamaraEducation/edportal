16e35eca621edbf57d9c96c48d475fa88ec39d0b
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-07 11:27:50 +0300

    export system data

---
+++ views/system/export.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta content="{{$_ENV['AUTHOR']}}" name="description">
+		<meta name="description" content="{{$_ENV['DESCRIPTION']}}">
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon.png">
+		<title>{{$app['name']}}</title>
+		@css('/assets/css/bootstrap')
+	</head>
+	<body>
+		<div class="w3-bar w3-black">
+			<a href="/" class="w3-bar-item w3-button"><img src="/assets/img/logo.png" style="height: 60px"></a>
+			<a href="/" class="w3-bar-item w3-hide-small pad-top w3-right">Home</a>
+		</div>
+		<div class="wrapper">
+
+		</div>
+</html>
