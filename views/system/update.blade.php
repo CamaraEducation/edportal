69aada15d0bf9f3c9a800f784f2938ca754917cc
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-08 10:52:31 +0300

    portal update

--- views/system/update.blade.php
+++ views/system/update.blade.php
@@ -7,16 +7,43 @@
 		<meta content="{{$_ENV['AUTHOR']}}" name="description">
 		<meta name="description" content="{{$_ENV['DESCRIPTION']}}">
 		<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon.png">
-		<title>UPDATE APPLICATION</title>
+		<title>Portal Update</title>
+        @js('/assets/js/typewriter')
 		@css('/assets/css/bootstrap')
+        <style>
+            body{
+                background-color: black;
+            }
+            
+            .mag{
+                margin-left: 70%;
+            }
+            .wrapper{
+                color : #81BC00;
+                width: 100%;
+                height: 100%;
+                padding: 0 30px;
+            }
+        </style>
 	</head>
 	<body>
 		<div class="w3-bar w3-black">
 			<a href="/" class="w3-bar-item w3-button"><img src="/assets/img/logo.png" style="height: 60px"></a>            
-			<p class="w3-bar-item w3-hide-small pad-top w3-right">Updating portal, wait ...</p>
-			<p class="w3-bar-item w3-hide-small pad-top w3-right">Update Complete</p>
+			<p class="w3-bar-item w3-hide-small pad-top mag" id="loading"><span
+                class="txt-rotate"
+                data-period="2000"
+                data-rotate='[ "updating portal", "please wait ...!"]'></span>
+            </p>
+			<p class="w3-bar-item w3-hide-small pad-top w3-right" id="loaded" style="display: none">Update Complete</p>
 		</div>
+        @php flush(); ob_flush(); @endphp
 		<div class="wrapper">
-            @php $ls = shell_exec('ls'); var_dump($ls); @endphp
+            <pre>@php $exec = shell_exec('git pull origin main'); echo $exec; flush(); ob_flush(); @endphp</pre>
 		</div>
+
+        <script>
+            document.getElementById("loading").style.display = "none";
+            document.getElementById("loaded").style.display = "block";
+        </script>
+    </body>
 </html>
