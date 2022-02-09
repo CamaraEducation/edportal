b24d352e1fffbc9b4407dde5166284d114fb40a2
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-09 14:37:22 +0300

    export data to excel files

--- views/system/export.blade.php
+++ views/system/export.blade.php
@@ -9,6 +9,7 @@
 		<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon.png">
 		<title>Export Data</title>
 		@css('/assets/css/style')
+        <script src="https://cdn.jsdelivr.net/gh/linways/table-to-excel@v1.0.4/dist/tableToExcel.js"></script>
 	</head>
     <style>
         .export{
@@ -18,7 +19,7 @@
 	<body>
 		<div class="bg-dark">
 			<a href="/" ><img src="/assets/img/logo.png" style="height: 60px"></a>
-			<a href="/" class="btn btn-success float-right export">
+			<a class="btn btn-success float-right export" onclick="exportReportToExcel(this)">
                 <i class="fa fa-upload"></i>
                 Export
             </a>
@@ -26,9 +27,8 @@
 		<div class="wrapper">
             @php 
                 $school = Portal::configs();
-                $applytics = PortalExport::apps();
-                $vidlytics = '';
-                $doclytics = '';
+                $applytics = array_merge(PortalExport::apps(), PortalExport::video(), PortalExport::doc());
+                $portlytics = PortalExport::all();
             @endphp
             <table class="table bordered" id="applets">
                 <thead class="thead-dark">
@@ -39,7 +39,6 @@
                         <th hidden>Region</th>
                         <th hidden>Country</th>
                         <th>applet</th>
-                        <th>uri</th>
                         <th>type</th>
                         <th>age</th>
                         <th>role</th>
@@ -54,11 +53,10 @@
                             <td hidden>{{$school['ownership']}}</td>
                             <td hidden>{{$school['region']}}</td>
                             <td hidden>{{$school['country']}}</td>
-                            <td>{{$analytic['applet']}}</td>
-                            <td>{{$analytic['uri']}}</td>
+                            <td>{{$analytic['title']}}</td>
                             <td>{{$analytic['con_type']}}</td>
-                            <td></td>
-                            <td></td>
+                            <td>{{$analytic['age'] - UsersControl::fetch_data($analytic['visitor'], 'user_age', 'data')}}</td>
+                            <td>{{role(UsersControl::fetch_data($analytic['visitor'], 'user_role', 'main'))}}</td>
                             <td>{{$analytic['time']}}</td>
                         </tr>
                     @endforeach
@@ -69,11 +67,12 @@
                 <thead class="thead-dark">
                     <tr>
                         <th>school</th>                        
-                        <th hidden>Category</th>
-                        <th hidden>Ownership</th>
-                        <th hidden>Region</th>
-                        <th hidden>Country</th>
+                        <th hidden>category</th>
+                        <th hidden>ownership</th>
+                        <th hidden>region</th>
+                        <th hidden>country</th>
                         <th>uri</th>
+                        <th>identifier</th>
                         <th>age</th>
                         <th>role</th>
                         <th>time</th>
@@ -81,18 +80,43 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>{{$school['school']}}</td>
-                        <td hidden>{{$school['category']}}</td>
-                        <td hidden>{{$school['ownership']}}</td>
-                        <td hidden>{{$school['region']}}</td>
-                        <td hidden>{{$school['country']}}</td>
-                    </tr>
+                    @foreach ($portlytics as $analytic)
+                        <tr>
+                            <td>{{$school['school']}}</td>
+                            <td hidden>{{$school['category']}}</td>
+                            <td hidden>{{$school['ownership']}}</td>
+                            <td hidden>{{$school['region']}}</td>
+                            <td hidden>{{$school['country']}}</td>
+                            <td>{{$analytic['uri']}}</td>
+                            <td>{{$analytic['identifier']}}</td>                            
+                            <td>{{$analytic['age'] - UsersControl::fetch_data($analytic['visitor'], 'user_age', 'data')}}</td>
+                            <td>{{role(UsersControl::fetch_data($analytic['visitor'], 'user_role', 'main'))}}</td>
+                            <td>{{$analytic['live']}}</td>
+                            <td>{{$analytic['time']}}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
 		</div>
 
         <script>
+
+		    function exportReportToExcel() {
+                let table = document.getElementsByTagName("table"); // you can use document.getElementById('tableId') as well by providing id to the table tag
+                TableToExcel.convert(table[0], { // html code may contain multiple tables so here we are refering to 1st table tag
+                    name: '{{"applets_analysis_".date("Y-m-d-h-i-s")}}.xlsx', // fileName you could use any name
+                    sheet: {
+                        name: 'Sheet 1' // sheetName
+                    }
+                });
+
+                TableToExcel.convert(table[1], { // html code may contain multiple tables so here we are refering to 1st table tag
+                    name: '{{"portal_analysis_".date("Y-m-d-h-i-s")}}.xlsx', // fileName you could use any name
+                    sheet: {
+                        name: 'Sheet 1' // sheetName
+                    }
+                });
+            }
 
         </script>
 </html>
