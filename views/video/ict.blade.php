b464ffd8ed2dde5c383d3e79fad1c71fc5a3ccd4
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-18 12:34:54 +0300

    index preloaded videos

---
+++ views/video/ict.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.app')
+@section('title', 'Videos')
+@section('header')   
+@endsection
+@section('header')
+    
+@endsection
+
+@section('content')
+    <div class="content-body">
+        <div class="container-fluid">
+            <iframe src="http://camara.portal/upload/video/ict/player.html" style="width:100%; height:100%; border:none; z-index:999999;">
+                Your browser doesn't support iframes
+            </iframe>
+        </div>
+    </div>    
+@endsection
+
+@section('footer')
+    
+@endsection
