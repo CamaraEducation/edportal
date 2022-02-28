b1f14dca9e2035525d10d035125b4bf814502079
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-02-28 13:49:31 +0300

    create the sync table, alter table subjects

--- controls/portal/update.php
+++ controls/portal/update.php
@@ -1,25 +1,30 @@
 <?php
 class PortalUpdate{
  	public static function init(){
-		$sql = 
-		"ALTER TABLE `users`
-			ADD COLUMN `user_skills` VARCHAR(100) NULL DEFAULT NULL AFTER `user_token`,
-			ADD COLUMN `user_interest` VARCHAR(100) NULL DEFAULT NULL AFTER `user_skills`,
-			ADD COLUMN `user_lang` VARCHAR(100) NULL DEFAULT NULL AFTER `user_interest`,
-			ADD COLUMN `user_class` VARCHAR(100) NULL DEFAULT NULL AFTER `user_lang`,
-			ADD COLUMN `user_age` VARCHAR(100) NULL DEFAULT NULL AFTER `user_class`;
-		";
+		 $sql = "CREATE TABLE `sync` (
+			`id` INT NOT NULL,
+			`site` BIGINT NOT NULL DEFAULT 0,
+			`app` BIGINT NOT NULL DEFAULT 0,
+			`vid` BIGINT NOT NULL DEFAULT 0,
+			`doc` BIGINT NOT NULL DEFAULT 0,
+			`last` DATETIME NOT NULL DEFAULT now() ON UPDATE now(),
+			PRIMARY KEY (`id`)
+		)
+		COLLATE='latin1_swedish_ci'";
 
-		if(mysqli_query(conn(), $sql)){echo "<b>SUCCESS:</b> Users' Table has been succesfully altered<br>";
-			}else{echo "<b style='color:brown'>FAILED:</b> Could not alter Users' Table<br>";
+		if(mysqli_query(conn(), $sql)){ echo "<b>SUCCESS:</b> The data sync table has been succesfully created";
+			}else{ echo "<b style='color:brown'>FAILED:</b> The data sync table has been succesfully created";
 		}
 
-		$sql = "DROP TABLE IF EXISTS userdata";
-		if(mysqli_query(conn(), $sql)){
-			echo "<b>SUCCESS:</b> Userdata Table has been succesfully dropped<br>";
-			}else{echo "<b style='color:brown'>FAILED:</b> Could not drop Userdata Table<br>";
-		}
 
+		$sql = "ALTER TABLE `subjects`
+		ADD COLUMN `thumb` VARCHAR(50) NULL DEFAULT '/upload/subject/thumb/def.jpg' AFTER `name`,
+		ADD COLUMN `cover` VARCHAR(50) NULL DEFAULT '/upload/subject/cover/def.png' AFTER `thumb`";
+
+		if(mysqli_query(conn(), $sql)){ echo "<b>SUCCESS:</b> Table subject was succesfully updated";
+			}else{ echo "<b style='color:brown'>FAILED:</b> Could not update table subject";
+		}
+		
 		echo "THE UPDATE IS COMPLETE";
  	}
 }
