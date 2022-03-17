f8b9a55758ca0a57b732e0cb94acc344fd0818c1
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-03-17 10:50:11 +0300

    update cotent

--- controls/portal/update.php
+++ controls/portal/update.php
@@ -1,28 +1,41 @@
 <?php
 class PortalUpdate{
  	public static function init(){
-		 $sql = "CREATE TABLE `sync` (
-			`id` INT NOT NULL,
-			`site` BIGINT NOT NULL DEFAULT 0,
-			`app` BIGINT NOT NULL DEFAULT 0,
-			`vid` BIGINT NOT NULL DEFAULT 0,
-			`doc` BIGINT NOT NULL DEFAULT 0,
-			`last` DATETIME NOT NULL DEFAULT now() ON UPDATE now(),
-			PRIMARY KEY (`id`)
-		)
-		COLLATE='latin1_swedish_ci'";
+		$version = Portal::fetch('last');
+		$current = file_get_contents("portal.json");
+		while($version != $current){
 
-		if(mysqli_query(conn(), $sql)){ echo "<b>SUCCESS:</b> The data sync table has been succesfully created</b><br>";
-			}else{ echo "<b style='color:brown'>FAILED:</b> The data sync table could not be created</b><br>";
-		}
+			switch($version){
+				case '0,0,0':
+					$sql = "CREATE TABLE `sync` (
+						`id` INT NOT NULL,
+						`site` BIGINT NOT NULL DEFAULT 0,
+						`app` BIGINT NOT NULL DEFAULT 0,
+						`vid` BIGINT NOT NULL DEFAULT 0,
+						`doc` BIGINT NOT NULL DEFAULT 0,
+						`last` DATETIME NOT NULL DEFAULT now() ON UPDATE now(),
+						PRIMARY KEY (`id`)
+					)
+					COLLATE='latin1_swedish_ci'";
+
+					if(mysqli_query(conn(), $sql)){ echo "<b>SUCCESS:</b> The data sync table has been succesfully created</b><br>";
+						}else{ echo "<b style='color:brown'>FAILED:</b> The data sync table could not be created</b><br>";
+					}
 
 
-		$sql = "ALTER TABLE `subjects`
-		ADD COLUMN `thumb` VARCHAR(50) NULL DEFAULT '/upload/subject/thumb/def.jpg' AFTER `name`,
-		ADD COLUMN `cover` VARCHAR(50) NULL DEFAULT '/upload/subject/cover/def.png' AFTER `thumb`";
+					$sql = "ALTER TABLE `subjects`
+					ADD COLUMN `thumb` VARCHAR(50) NULL DEFAULT '/upload/subject/thumb/def.jpg' AFTER `name`,
+					ADD COLUMN `cover` VARCHAR(50) NULL DEFAULT '/upload/subject/cover/def.png' AFTER `thumb`";
 
-		if(mysqli_query(conn(), $sql)){ echo "<b>SUCCESS:</b> Table subject was succesfully updated</b><br>";
-			}else{ echo "<b style='color:brown'>FAILED:</b> Could not update table subject</b><br>";
+					if(mysqli_query(conn(), $sql)){ echo "<b>SUCCESS:</b> Table subject was succesfully updated</b><br>";
+						}else{ echo "<b style='color:brown'>FAILED:</b> Could not update table subject</b><br>";
+					}
+					break;
+
+				case '121':
+					// do nothing
+					 
+			}
 		}
 		
 		echo "THE UPDATE IS COMPLETE";
