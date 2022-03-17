48d66d91a19b734f1068c0b17ff85bc5878258ac
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-03-17 10:51:27 +0300

    support boards

---
+++ views/system/support/et.blade.php
@@ -0,0 +1,84 @@
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <h3 class="text-primary pad-1">Pysical Address</h3>
+            <ul class="pad-half bold">
+                <li>
+                    <i class="fa fa-building"></i> : 
+                    <span class="italic">Dama House, Office No. 304,</span>
+                </li>
+                <li>
+                    <i class="fa fa-location-arrow"></i> :                    
+                    <span class="italic">Lideta, Addis Ababa</span>
+                </li>
+                <li>
+                    <i class="fa fa-envelope"></i> :                     
+                    <span class="italic">Ethiopia</span>
+                </li>
+            </ul>
+            <div class="space"></div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+            <h3 class="text-primary pad-1">Virtual Address</h3>
+            <ul class="pad-half bold">
+                <li>
+                    <i class="fa fa-phone-square"></i> : 
+                    <span class="italic">251 (0) 11869051</span>
+                </li>
+                <li>
+                    <i class="fa fa-envelope"></i> :                    
+                    <span class="italic" mail-to="tanzania@camara.org">ethiopia@camara.org</span>
+                </li>
+                <li>
+                    <i class="fa fa-safari"></i> :                     
+                    <span class="italic" ><a class="text-primary" href="//camara.org/ethiopia">camara.org/ethiopia</a></span>
+                </li>
+            </ul>
+            <div class="space"></div>
+        </div>
+    </div>
+
+    <div class="col-md-12 col-sm-12">
+        <div class="card">
+            <h3 class="text-primary pad-1 text-center">Social Presence</h3>
+            <center>
+                <div class="row">
+                    <div class="col">
+                        <a href="https://www.facebook.com/camaraethiopia/">
+                            <img src="/assets/icons/socials/fb.png" width="60px" alt="facebook">
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="https://et.linkedin.com/company/camara-education-ethiopia">
+                            <img src="/assets/icons/socials/in.png" width="60px" alt="Linkedin">
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="https://twitter.com/ethiopiacamara">
+                            <img src="/assets/icons/socials/twitter.png" width="60px" alt="twitter">
+                        </a>
+                    </div>
+                </div>
+            </center>
+            <div class="space"></div>
+        </div>
+    </div>
+
+    <div class="col-md-12">
+        <div class="card row">
+            <div class="col-md-6">
+                <h3 class="text-primary pad-1">Camtics</h3>
+                <p class="pad-half bold">Camara Ticketing and Support Center</p>
+                <p class="pad-half">Camtics (in-shorts) is an online platform that Camara uses to solves issue with its customers, Users can create tickets and track the progress or the status of the issue that they face. <br>
+                    Click <a href="//camtics.camara.org" class="text-primary bold">here</a> to <a href="//camtics.camara.org" class="text-primary bold">open a ticket</a>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="/upload/team/et.webp" style="padding-top: 10px; border-radius: 15px; width:100%" alt="camtics">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/system/support/zb.blade.php
@@ -0,0 +1,79 @@
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <h3 class="text-primary pad-1">Pysical Address</h3>
+            <ul class="pad-half bold">
+                <li>
+                    <i class="fa fa-building"></i> : 
+                    <span class="italic">c/o Educational Broadcasting Services,</span>
+                </li>
+                <li>
+                    <i class="fa fa-location-arrow"></i> :                    
+                    <span class="italic">Ministry of General Education, Fairley Road,</span>
+                </li>
+                <li>
+                    <i class="fa fa-envelope"></i> :                     
+                    <span class="italic">Lusaka, Zambia</span>
+                </li>
+            </ul>
+            <div class="space"></div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+            <h3 class="text-primary pad-1">Virtual Address</h3>
+            <ul class="pad-half bold">
+                <li>
+                    <i class="fa fa-phone-square"></i> : 
+                    <span class="italic">260 (0) 969 634712</span>
+                </li>
+                <li>
+                    <i class="fa fa-envelope"></i> :                    
+                    <span class="italic" mail-to="techsupportzambia@camara.org">techsupportzambia@camara.org</span>
+                </li>
+                <li>
+                    <i class="fa fa-safari"></i> :                     
+                    <span class="italic" ><a class="text-primary" href="//camara.org/zambia">camara.org/zambia</a></span>
+                </li>
+            </ul>
+            <div class="space"></div>
+        </div>
+    </div>
+
+    <div class="col-md-12 col-sm-12">
+        <div class="card">
+            <h3 class="text-primary pad-1 text-center">Social Presence</h3>
+            <center>
+                <div class="row">
+                    <div class="col">
+                        <a href="https://www.facebook.com/camarazambia">
+                            <img src="/assets/icons/socials/fb.png" width="60px" alt="facebook">
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="https://ie.linkedin.com/company/camara-education-ltd-">
+                            <img src="/assets/icons/socials/in.png" width="60px" alt="Linkedin">
+                        </a>
+                    </div>
+                </div>
+            </center>
+            <div class="space"></div>
+        </div>
+    </div>
+
+    <div class="col-md-12">
+        <div class="card row">
+            <div class="col-md-6">
+                <h3 class="text-primary pad-1">Camtics</h3>
+                <p class="pad-half bold">Camara Ticketing and Support Center</p>
+                <p class="pad-half">Camtics (in-shorts) is an online platform that Camara uses to solves issue with its customers, Users can create tickets and track the progress or the status of the issue that they face. <br>
+                    Click <a href="//camtics.camara.org" class="text-primary bold">here</a> to <a href="//camtics.camara.org" class="text-primary bold">open a ticket</a>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="/upload/team/zb.webp" style="padding-top: 10px; border-radius: 15px; width:100%" alt="camtics">
+            </div>
+        </div>
+    </div>
+</div>
