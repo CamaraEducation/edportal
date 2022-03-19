d64b744dc768dab5b7dd2877c7f0716eed613041
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-03-19 17:50:30 +0300

    update portal using ajax

--- views/system/update.blade.php
+++ views/system/update.blade.php
@@ -1,31 +1,28 @@
 @extends('layout.app')
-@section('title', 'Video Category')
+@section('title', 'Update system')
 @section('header')
 <style>
-	.update-btn{
-        background-color: #81BC00;
-        color: white;
-        border: none;
-        padding: 10px;
-        border-radius: 5px;
-        margin-top: 10px;
-        margin-left: 10px;
-    }
-
-    .update-btn:hover{
-        background-color: #81BC00;
-        color: white;
-        border: none;
-        padding: 10px;
-        border-radius: 5px;
-        margin-top: 10px;
-        margin-left: 10px;
-    }
-
     .update{
         background: black;
         max-width: 560px;
-        color: white; 
+        color: rgb(121, 212, 107);
+        min-height: 300px;
+        border-radius: 10px;
+    }
+
+    .log{
+        background: #9d9e9f;
+        width: 95%;
+        color: black;
+        min-height: 100px;
+        border-radius: 10px;
+    }
+
+    pre{
+        color: #72a54d !important;
+        font-weight: bold !important;
+        text-align: left;
+        overflow-wrap: anywhere;
     }
 </style>
 @endsection
@@ -33,15 +30,87 @@
 @section('content')
 	<div class="content-body">
 		<div class="container-fluid">
-			<center>
-                <div class="update">
-
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <center>
+                        <button id="update" class="btn btn-lg btn-primary">Start Update</button>
+                    </center>
+                    <br>
+                    <div class="log">
+                        <ol class="pad-half" id="log">
+                            
+                        </ol>
+                    </div>
                 </div>
-            </center>
+                <div class="col-sm-12 col-md-8">
+                    <center>
+                        <div class="update pad-half" id="progress">
+                            
+                        </div>
+                </center>
+                </div>
+            </div>
 		</div>
 	</div>  
 @endsection
 
 @section('footer')
+
+<script>
+    //when the button update is clicked
+    $('#update').click(function(){
+        var x = '';
+        $('#update').prop('disabled', true);
+        $('#update').html('please wait')
+        
+        //append the log to log list
+        $('#log').append('<li>Starting Update</li>');
+        $('#progress').append('<pre>Commencing update</pre>');
+        
+        sleep(1000);
+        $('#log').append('<li>Stashing Changes</li>');
+
+        $.ajax({
+            url: '/update/portal/stash',
+            type: 'POST',
+            data: x,
+            success: function(data){
+                if(data != ''){
+                    $('#progress').append(data);
+                }
+            }
+        });
+
+        sleep(1000);
+        $('#log').append('<li>Pulling Changes/li>');
+        $.ajax({
+            url: '/update/portal/pull',
+            type: 'POST',
+            data: x,
+            success: function(data){
+                if(data != ''){
+                    $('#progress').append(data);
+                }
+            }
+        });
+
+        sleep(1000);
+        $('#log').append('<li>Installing Changes</li>');
+        $.ajax({
+            url: '/update/portal/apply',
+            type: 'POST',
+            data: x,
+            success: function(data){
+                if(data != ''){
+                    $('#progress').append(data);
+                }
+            }
+        });
+
+        //enable the button
+        $('#update').html('Update Complete');
+
+    });
+</script>
 
 @endsection
