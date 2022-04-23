982a7b799e11521992291f9e48641cf71b108966
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-04-23 14:22:29 +0300

    check server connection

--- views/system/connect/connsum.blade.php
+++ views/system/connect/connsum.blade.php
@@ -4,10 +4,3 @@
 @else
     <p class="text=red">Connection to the portal Synchronisation server is not available</p>
 @endif
-
-<h3 class="text-primary">Computer usage and Data Tracking</h3>
-@if(is_connected() == true)
-    <p class="text=green">The Portal is Synchronising well to server address {{sync_conn()}}</p>
-@else
-    <p class="text=red">Connection to the portal Synchronisation server is not available</p>
-@endif
--- views/system/connect/nmsdata.blade.php
+++ views/system/connect/nmsdata.blade.php
@@ -1,13 +1,15 @@
-<h3 class="text-primary">Portal Syncronisation</h3>
-@if(is_connected() == true)
-    <p class="text=green">The Portal is Synchronising well to server address {{sync_conn}}</p>
-@else
-    <p class="text=red">Connection to the portal Synchronisation server is not available</p>
-@endif
-
-<h3 class="text-primary">Computer usage and Data Tracking</h3>
-@if(is_connected() == true)
-    <p class="text=green">The Portal is Synchronising well to server address {{sync_conn}}</p>
-@else
-    <p class="text=red">Connection to the portal Synchronisation server is not available</p>
-@endif
+@php $nms = NMSController::check(); @endphp
+<h3 class="text-primary">NMS Test</h3>
+<li>Number of Computers : <span class="text-bold" style="font-weight: 600">{{$nms['count']}}</span></li> <br>
+<table class="table">
+    <thead class="thead-light">
+        <th>Mac Adrress</th>
+    </thead>
+    <tbody>
+        @foreach ($nms['macs'] as $mac)
+        <tr>
+            <td>{{$mac['mac_add']}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
