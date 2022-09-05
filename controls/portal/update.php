0188354f650510f3ab1cce48bd817e013b4db325
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-09-05 13:02:07 +0300

    update

--- controls/portal/update.php
+++ controls/portal/update.php
@@ -1,7 +1,29 @@
 <?php
 class PortalUpdate{
- 	public static function init(){
-		echo "system version is now 1.5.2";
+ 	public static function init(){ 
+		$no = 0;
+		// create dropbox Table
+		$sql[0] = "CREATE TABLE IF NOT EXISTS `dropbox` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`name` varchar(30) NOT NULL,
+			`description` text NOT NULL,
+			`path` text NOT NULL,
+			`type` varchar(10) NOT NULL,
+			`created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			PRIMARY KEY (`id`)
+		   ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+
+		$res[0] = ['success' => 'Table dropbox created successfully', 'error' => 'Error creating table dropbox: '];
+
+		foreach($sql as $query):
+			if(mysqli_query(conn(), $query)):
+				echo $res[$no]['success'].PHP_EOL;
+			else:
+				echo $res[$no]['error'] . mysqli_error(conn()).PHP_EOL;
+			endif; $no++;
+		endforeach;
+
+		echo "system version is now 1.5.3";
  	}
 
 	public static function auto(){
