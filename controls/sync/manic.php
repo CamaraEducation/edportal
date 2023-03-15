ae1c0e6cd3adca699aad1e5e02c02ec95aee07c4
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2023-03-15 09:15:59 +0300

    return server output after data is received

--- controls/sync/manic.php
+++ controls/sync/manic.php
@@ -77,6 +77,8 @@
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $server_output = curl_exec($ch);
             curl_close ($ch);
+
+            return $server_output;
             
         }
 
