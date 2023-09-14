624d4102ef340e4a2a74c769d3a63b6bcab3272f
ibnsultan <abdulbasitsultan4@gmail.com>
2023-09-14 09:57:37 +0300

    ✨Add: safeguarding table

--- controls/portal/update.php
+++ controls/portal/update.php
@@ -119,50 +119,9 @@ class PortalUpdate{
 			],
 		];
 
-		# alter uptime to bigint from varchar
-		$nms[0] = "ALTER TABLE `process` CHANGE COLUMN `user_time` `user_time` BIGINT NOT NULL DEFAULT 0 COLLATE 'utf8_unicode_ci' AFTER `up_time`";
-
-		# create process view in ccnms
-		$nms[1] = "CREATE OR REPLACE VIEW process_sum AS(
-			SELECT id, host_name, name,
-				CAST( SUBSTRING(FROM_UNIXTIME(substring(start_time, 1, 10)), 1, 19) AS DATETIME) AS started,
-				MAX(user_time) AS uptime
-			FROM `process` 
-			WHERE 
-				`name` NOT IN ('0install', '0install-win', 'snapd', 'systemd-udevd', 'svchost', 'xorg', 'sppsvc', 'mscorsvw', 'mysqld') AND
-				host_name != 'camara2004' AND
-				user_time>1000
-			GROUP by start_time, name, host_name
-			ORDER BY id ASC
-		)";
-
-		$nms[3] = "CREATE OR REPLACE VIEW pc_usage AS (
-			SELECT id, host_name, CAST(booted AS DATETIME) AS booted, MAX(TIME_TO_SEC(SUBSTRING_INDEX(uptime, ' ' , '-1'))) AS uptime 
-			FROM os_info
-			WHERE host_name != 'camara2004'
-			GROUP BY host_name, CAST(booted AS DATETIME)
-			ORDER BY id ASC
-		)";
-
-		$nms[4] = "CREATE TABLE IF NOT EXISTS `sync` (
-			`id` INT NOT NULL AUTO_INCREMENT,
-			`usage` BIGINT NOT NULL DEFAULT 0,
-			`process` BIGINT NOT NULL DEFAULT 0,
-			PRIMARY KEY (`id`)
-		) COLLATE='latin1_swedish_ci'";
-
 		#applying portal queries
 		foreach($sql as $query):
 			if(mysqli_query(conn(), $query)):
-				echo $res[$no]['success'].'<br>';
-			else:
-				echo $res[$no]['error'] . '<br>';
-			endif; $no++;
-		endforeach;
-
-		# applying nms queries
-		foreach($nms as $query):
-			if(mysqli_query(ccnms(), $query)):
 				echo $res[$no]['success'].'<br>';
 			else:
 				echo $res[$no]['error'] . '<br>';
@@ -196,10 +155,6 @@ class PortalUpdate{
 			exec('sudo unzip -o /www/wwwroot/default/upload/dropbox/manic.zip -d /www/wwwroot/');
 			echo '<b class="text-success">SUCCESS: </b>Manic syncronization files updated successfully <br>'.PHP_EOL;
 
-			exec("sudo cp -f /www/wwwroot/config.log /www/wwwroot/default/manic/");
-			exec('sudo chmod -R 777 /www/wwwroot/default/manic');			
-			echo '<b class="text-success">SUCCESS: </b>Manic syncronization files Permisions updated successfully <br>'.PHP_EOL;
-
 			/*$directory = '/www/wwwroot/manic/data/jobs';
 
 			# log existing job_files to manic_jobs table
@@ -216,16 +171,46 @@ class PortalUpdate{
 				}
 			} */
 			
-			db()->query("UPDATE config set `last` = 222 where id = 1");
+			db()->query("UPDATE config set `last` = 223 where id = 1");
 
 		endif;
 
+		if(ConfigsController::get('last') == 223):
+
+			db()->query("
+				CREATE TABLE IF NOT EXISTS `safeguarding` (
+				`id` int(11) NOT NULL AUTO_INCREMENT,
+				`title` text NOT NULL,
+				`content` text NOT NULL,
+				`type` char(50) NOT NULL DEFAULT '',
+				`country` char(50) NOT NULL DEFAULT '',
+				`updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+				`created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+				PRIMARY KEY (`id`)
+				) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+			");
+
+			db()->query("
+				DELETE FROM `safeguarding`;
+				INSERT INTO `safeguarding` (`id`, `title`, `content`, `type`, `country`, `updated`, `created`) VALUES
+					(1, 'Protecting our most vulnerable', '202312090129.txt', 'text', 'all', '2023-09-13 18:20:12', '2023-09-13 13:36:30'),
+					(2, 'SCI Safeguarding & Digital Technology Tipsheet', '202312090129.pdf', 'doc', 'all', '2023-09-13 13:33:18', '2023-09-13 13:33:18'),
+					(3, 'SCI_Safeguarding & Social Media (Field-Friendly Guidance)_EN', '202312091658.pdf', 'doc', 'all', '2023-09-13 17:01:12', '2023-09-13 17:01:12'),
+					(4, 'Let us Talk pants', '202312090139.txt', 'text', 'all', '2023-09-13 18:20:29', '2023-09-13 18:10:52');
+			");
+
+			db()->query("UPDATE config set `last` = 224 where id = 1");
+		endif;
+
+		
+		exec("sudo cp -f /www/wwwroot/config.log /www/wwwroot/default/manic/");
+		exec('sudo chmod -R 777 /www/wwwroot/default/manic');
 		
 		// drop manic jobs table if exists
 		db()->query("DROP TABLE IF EXISTS `manic_jobs`");
 
 		
-		echo "system version is now 2.2.2";
+		echo "system version is now 2.2.4";
  	}
 
 	public static function auto(){
