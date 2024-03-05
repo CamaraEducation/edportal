72edcb3c6f1fcd964fbbef48cb4273ccf4dddc95
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-05 08:25:06 +0300

    use current execution path to determine storage directory

--- controls/upload/upload.php
+++ controls/upload/upload.php
@@ -1,7 +1,7 @@
 <?php
 class FileUploader{
     public static function upload($file='media', $dir=''){
-        $target_dir = upload . $dir;
+        $target_dir = getcwd(). "/uploads/$dir";
         $imageFileType = strtolower(pathinfo($_FILES[$file]["name"],PATHINFO_EXTENSION));
         $_FILES["media"]['name'] = round(microtime(true)) . rand(1111, 99999) . '.' . $imageFileType;
         $target_file = $target_dir . basename($_FILES["media"]["name"]); $uploadOk = 1;
