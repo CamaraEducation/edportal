7c419b02e2453ad1de3e3964f7dcb9467456ca2e
ibnsultan <abdulbasitsultan4@gmail.com>
2024-05-17 15:00:28 +0300

    Hotfix: wrong upload directory

--- controls/upload/upload.php
+++ controls/upload/upload.php
@@ -1,7 +1,7 @@
 <?php
 class FileUploader{
     public static function upload($file='media', $dir=''){
-        $target_dir = getcwd(). "/uploads/$dir";
+        $target_dir = getcwd(). "/upload/$dir";
         $imageFileType = strtolower(pathinfo($_FILES[$file]["name"],PATHINFO_EXTENSION));
         $_FILES["media"]['name'] = round(microtime(true)) . rand(1111, 99999) . '.' . $imageFileType;
         $target_file = $target_dir . basename($_FILES["media"]["name"]); $uploadOk = 1;
