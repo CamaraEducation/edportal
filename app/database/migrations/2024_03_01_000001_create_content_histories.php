7ab530b8c09543d72084d9adb40e659639bdc9c4
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-10-06 10:28:16 +0300

    feat: migration for content history table

---
+++ app/database/migrations/2024_03_01_000001_create_content_histories.php
@@ -0,0 +1,34 @@
+<?php
+
+use Leaf\Schema;
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateContentHistory extends Database
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (!static::$capsule::schema()->hasTable('content_history')) :
+            static::$capsule::schema()->create('content_history', function (Blueprint $table) {
+                $table->integer('user_id')->unsigned();
+                $table->json('tag_history');
+                $table->timestamp('created_at')->useCurrent();
+                $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+                $table->primary('user_id');
+            });
+        endif;
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists('content_histories');
+    }
+}
