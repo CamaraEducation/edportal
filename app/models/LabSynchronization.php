8a82b2ce5f0d32362b005702b1b42e23a48f94d3
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-10-06 10:31:21 +0300

    feat: add LabSynchronization model

---
+++ app/models/LabSynchronization.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Leaf\Database as DB;
+
+class LabSynchronization extends Model
+{
+    protected $table = 'lab_synchronization';
+    protected $fillable = ['key', 'value'];
+    public $timestamps = true;
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+    public static function remainingData(){
+        return DB::$capsule::table('lab_usage as u')
+            ->join('lab_synchronization as s', function($join) {
+            $join->on('s.key', '=', DB::$capsule::raw("'general_usage'"))
+                 ->whereColumn('u.id', '>', 's.value');
+            })
+            ->count('s.id');
+    }
+}
