73efc99829b97bc9235eb003a36c425368283013
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-10-13 11:44:52 +0300

    add: platform stats

--- app/controllers/App/HomeController.php
+++ app/controllers/App/HomeController.php
@@ -4,6 +4,11 @@ namespace App\Controllers\App;
 
 use App\Controllers\Controller;
 
+use App\Models\Video;
+use App\Models\Document;
+use App\Models\Slide;
+// use App\Models\Notes;
+
 class HomeController extends Controller
 {
     public function __construct()
@@ -14,15 +19,38 @@ class HomeController extends Controller
     public function home(){
 
         $this->data->title = 'Dashboard';
-        $this->data->current_user = auth()->user();
+        $this->contentStats = $this->portalStats();
 
-        render('app.home', (array) $this->data);
+        render('app.dashboard.home', $this->data);
+        //dd($this->data);
+        // render('welcome');
+    }
+
+    public function portalStats(){
+        return [
+            'videos' => [
+                'icon' => 'ph-video text-primary',
+                'total' => Video::count()
+            ],
+            'documents' => [
+                'icon' => 'ph-book text-warning',
+                'total' => Document::count()
+            ],
+            'slides' => [
+                'icon' => 'ph-cards text-info',
+                'total' => Slide::count()
+            ],
+            'notes' => [
+                'icon' => 'ph-pencil-circle text-danger',
+                'total' => 0
+            ]
+        ];
     }
 
     public function start(){ 
         $this->data->title = 'Start';
         $this->data->current_user = auth()->user();
 
-        render('app.start', (array) $this->data);
+        render('app.start', $this->data);
     }
 }
