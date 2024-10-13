c411814bc277b21800c3c2d0a10b64cef37bba6d
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-10-13 11:49:58 +0300

    refactor: Remove unused routes and update login route in auth.php

--- app/routes/auth.php
+++ app/routes/auth.php
@@ -4,31 +4,18 @@ use App\Controllers\AuthController;
 
 app()->redirect('/login', '/auth/login');
 app()->redirect('/reset', '/auth/reset');
-app()->redirect('/register', '/auth/register');
 
 app()->group('auth', function(){
 
-    app()->get('/login', 'AuthController@signin');
-    app()->get('/reset', 'AuthController@forgot');
-    app()->get('/register', 'AuthController@signup');
-
-    app()->post('/login', 'AuthController@login');
-    app()->post('/reset', 'AuthController@reset');
-    app()->post('/register', 'AuthController@register');
-
-    app()->get('/password/{token}', 'AuthController@changePassword');
-    app()->post('/password/{token}', 'AuthController@updatePassword');
+    app()->get('/login', ['name'=>'login', 'AuthController@signin']);
+    app()->post('/login', ['name'=>'login', 'AuthController@login']);
 
     app()->get('/logout', ['name'=>'logout', 'AuthController@logout']);
 
 });
 
 app()->group('api', function(){
-
     app()->group('/auth', function(){
-
-        app()->post('/login', fn() => AuthController::login(true));
-
+        app()->post('/login', ['name'=>'apiLogin', 'AuthController@login']);
     });
-
 });
