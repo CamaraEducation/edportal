9e6248f035996f7bde095094e4ab12557c428333
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-12-01 00:10:31 +0300

    feat: add support for blade.css extension and implement @style directive for stylesheets

--- lib/Blade.php
+++ lib/Blade.php
@@ -14,12 +14,12 @@ function bladeInit(){
             
             // Add .blade.js extension
             $this->blade->addExtension('blade.js', 'blade');
+            $this->blade->addExtension('blade.css', 'blade');
     
-            foreach(get_class_methods($this) as $method){
-                if(strpos($method, 'register') !== false){
-                    $this->$method();
-                }
-            }
+            // Register custom directives
+            $this->registerRouteDirective();
+            $this->registerScriptDirective();
+            $this->registerStyleDirective();
         }
     
         protected function getViewPath()
@@ -52,32 +52,84 @@ function bladeInit(){
                     return <<<HTML
                     <?php
                         \$__env->startPush('scripts');
-                        echo '<script src="' . ViewsPath('$view') . '"></script>' . PHP_EOL;
+                        echo '<script src="' . '$view' . '"></script>' . PHP_EOL;
                         \$__env->stopPush();
                     ?>
                     HTML;
                 }
-            
-                $scriptTag = "<script>";
-                if($type == 'module') {
-                    $scriptTag = "<script type=\'module\'>";
+
+                elseif($type == 'srcmodule'){
+                    return <<<HTML
+                    <?php
+                        \$__env->startPush('scripts');
+                        echo '<script type="module" src="' . '$view' . '"></script>' . PHP_EOL;
+                        \$__env->stopPush();
+                    ?>
+                    HTML;
                 }
+
+                // if($type == 'module') $scriptTag = "<script type=\'module\'>";
+                $scriptTag = (function() use ($type){
+                    if($type == 'module') return "<script type='module'>";
+                    if($type == 'importmap') return "<script type='importmap'>";
+                    return "<script>";
+                })();
             
                 return <<<HTML
                 <?php
                     \$view = str_replace('.', '/', '$view');
                     \$filePath = trim(ViewsPath(\$view . '.blade.js'), '//');
-                    if (file_exists(\$filePath)) {
+                    // if (file_exists(\$filePath)) {
                         \$__env->startPush('scripts');
                         echo '$scriptTag' . PHP_EOL;
                         echo \$__env->make('$view', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render() . PHP_EOL;
                         echo chr(60) . "/script" . chr(62) . PHP_EOL;
                         \$__env->stopPush();
-                    }
+                    //}else{
+                        //echo "<!-- Blade.js file not found: \$filePath -->";
+                    //}
                 ?>
                 HTML;
             });
             
+        }
+
+        public function registerStyleDirective()
+        {
+            // Define @style directive
+            $this->blade->compiler()->directive('style', function ($expression) {
+                $params = explode(',', trim($expression, "()'\""));
+                $view = trim($params[0], "'\" ");
+                $type = isset($params[1]) ? trim($params[1], "'\" ") : 'style';
+
+                if($type == 'src'){
+                    return <<<HTML
+                    <?php
+                        \$__env->startPush('styles');
+                        echo '<link rel="stylesheet" href="' . '$view' . '">' . PHP_EOL;
+                        \$__env->stopPush();
+                    ?>
+                    HTML;
+                }
+            
+                $styleTag = "<style>";
+            
+                return <<<HTML
+                <?php
+                    \$view = str_replace('.', '/', '$view');
+                    \$filePath = trim(ViewsPath(\$view . '.blade.css'), '//');
+                    // if (file_exists(\$filePath)) {
+                        \$__env->startPush('styles');
+                        echo '$styleTag' . PHP_EOL;
+                        echo \$__env->make('$view', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render() . PHP_EOL;
+                        echo chr(60) . "/style" . chr(62) . PHP_EOL;
+                        \$__env->stopPush();
+                    //}else{
+                        //echo "<!-- Blade.css file not found: \$filePath -->";
+                    //}
+                ?>
+                HTML;
+            });
         }
     
         public function getBladeInstance()
