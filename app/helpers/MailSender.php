9cc071b89b54ab3b62c640a4dcdb1b5d3e16471b
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-30 23:59:28 +0300

    refactor: update environment variable retrieval in MailSender to use _env function

--- app/helpers/MailSender.php
+++ app/helpers/MailSender.php
@@ -12,11 +12,11 @@ class MailSender
     public function __construct()
     {
         $this->mailer = new SmtpMailer(
-            host: getenv('mail_host'),
-            username: getenv('mail_username'),
-            password: getenv('mail_password'),
-            encryption: getenv('mail_encryption'),
-            port: intval(getenv('mail_port'))
+            host: _env('MAIL_HOST'),
+            username: _env('MAIL_USERNAME'),
+            password: _env('MAIL_PASSWORD'),
+            encryption: _env('MAIL_ENCRYPTION'),
+            port: intval(_env('MAIL_PORT'))
         );
     }
 
@@ -34,7 +34,7 @@ class MailSender
     public function send($subject, $body, $recipientEmail, $recipientName, $senderName = null)
     {
         $mail = new Nette\Mail\Message;
-        $mail->setFrom($senderName ?? getenv('mail_username'))
+        $mail->setFrom($senderName ?? _env('MAIL_USERNAME'))
             ->addTo($recipientEmail, $recipientName)
             ->setSubject($subject)
             ->setBody($body);
@@ -55,7 +55,7 @@ class MailSender
     public function sendToAll($subject, $body, $recipients, $senderName = null)
     {
         $mail = new Nette\Mail\Message;
-        $mail->setFrom($senderName ?? getenv('mail_username'))
+        $mail->setFrom($senderName ?? _env('MAIL_USERNAME'))
             ->setSubject($subject)
             ->setBody($body);
 
@@ -78,7 +78,7 @@ class MailSender
     public function sendHtml($subject, $body, $recipientEmail, $recipientName, $senderName = null)
     {
         $mail = new Nette\Mail\Message;
-        $mail->setFrom($senderName ?? getenv('mail_username'))
+        $mail->setFrom($senderName ?? _env('MAIL_USERNAME'))
             ->addTo($recipientEmail, $recipientName)
             ->setSubject($subject)
             ->setHtmlBody($body);
