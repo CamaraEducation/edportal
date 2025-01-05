206d2bb7af25a25887c72d7298e8d133ef564705
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2025-01-05 11:27:11 +0300

    feat: add home view template with basic layout structure

---
+++ app/views/app/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')	
+@section('content')
+	<div class="pc-container">
+		<div class="pc-content">
+			<div class="container-fluid">
+
+
+
+            </div>			
+		</div>
+	</div>
+@endsection
