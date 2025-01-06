a354e17c385b934404e79103e46984bda3dac008
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2025-01-06 22:27:20 +0300

    refactor: add documentation and feat: porta;s controller

--- config/portal.php
+++ config/portal.php
@@ -1,6 +1,17 @@
 <?php
 
+/**
+ * This file contains the portal configurations.
+ */
+
 return [
-
-
+    /*
+    |--------------------------------------------------------------------------
+    | LAN or Online
+    |--------------------------------------------------------------------------
+    | This sets the environment of the application
+    | 
+    */
+    
+    'lan' => true,
 ];
